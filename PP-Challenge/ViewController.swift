0509b26e88b0e3332a5f3d8c7efd7c66bad893cb
Cookiezby <zhubingyi1992@gmail.com>
2020-04-08 15:47:32 +0900

    init commit

---
+++ PP-Challenge/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PP-Challenge
+//
+//  Created by 朱冰一 on 2020/04/08.
+//  Copyright © 2020 朱冰一. All rights reserved.
+//
+
+import UIKit
+import ReactiveSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+target 'PP-Challenge' do
+  use_frameworks!
+  pod 'ReactiveSwift', '~> 6.1'
+  pod 'ReactiveCocoa', '~> 10.1'
+
+  target 'PP-ChallengeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PP-ChallengeUITests' do
+    # Pods for testing
+  end
+
+end
